2abd3422b006f0cec186149648fb0c123e59be56
AK <akgupta3522@gmail.com>
2024-12-27 18:56:22 +0530

    momgo added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-
+//mongodb://127.0.0.1:27017/authuser
 async function connectDB() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/authuser').then(()=>{
+  await mongoose.connect(process.env.MONGO_URI).then(()=>{
       console.log("connection to db")
   }).catch((err)=>{
       console.log(err)
